302a99dd40e8313e4f49d622b5551a521120d3ff
Riccardo Strina <rstrina@amazon.com>
2025-07-24 09:38:04 +0200

    Update build.rs

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #endif
 
-#define LANGUAGE_VERSION 14
+#define LANGUAGE_VERSION 15
 #define STATE_COUNT 193
 #define LARGE_STATE_COUNT 2
 #define SYMBOL_COUNT 62
@@ -998,7 +998,7 @@ static bool ts_lex(TSLexer *lexer, TSStateId state) {
   }
 }
 
-static const TSLexMode ts_lex_modes[STATE_COUNT] = {
+static const TSLexerMode ts_lex_modes[STATE_COUNT] = {
   [0] = {.lex_state = 0, .external_lex_state = 1},
   [1] = {.lex_state = 0, .external_lex_state = 2},
   [2] = {.lex_state = 0, .external_lex_state = 3},
@@ -3790,6 +3790,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_jsp(void) {
     .state_count = STATE_COUNT,
     .large_state_count = LARGE_STATE_COUNT,
     .production_id_count = PRODUCTION_ID_COUNT,
+    .supertype_count = SUPERTYPE_COUNT,
     .field_count = FIELD_COUNT,
     .max_alias_sequence_length = MAX_ALIAS_SEQUENCE_LENGTH,
     .parse_table = &ts_parse_table[0][0],
@@ -3813,6 +3814,13 @@ TS_PUBLIC const TSLanguage *tree_sitter_jsp(void) {
       tree_sitter_jsp_external_scanner_deserialize,
     },
     .primary_state_ids = ts_primary_state_ids,
+    .name = "jsp",
+    .max_reserved_word_set_size = 0,
+    .metadata = {
+      .major_version = 0,
+      .minor_version = 1,
+      .patch_version = 0,
+    },
   };
   return &language;
 }
