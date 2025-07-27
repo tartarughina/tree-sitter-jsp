286b303caf986f61f6d976dbe940e39606b39626
Riccardo Strina <rstrina@amazon.com>
2025-07-27 20:28:54 +0200

    Add Java and EL injection support for JSP files

--- queries/injections.scm
+++ queries/injections.scm
@@ -1,4 +1,4 @@
-; JavaScript injection in script tags with type attribute
+; JavaScript injection in script tags with explicit type attribute
 ((script_element
   (start_tag
     (attribute
@@ -14,7 +14,7 @@
   (raw_text) @injection.content)
  (#set! injection.language "javascript"))
 
-; CSS injection in style tags with type attribute  
+; CSS injection in style tags with explicit type attribute  
 ((style_element
   (start_tag
     (attribute
@@ -29,3 +29,22 @@
 ((style_element
   (raw_text) @injection.content)
  (#set! injection.language "css"))
+
+; Java injection in JSP scriptlets - these contain Java code blocks
+(jsp_scriptlet) @injection.content
+(#set! injection.language "java")
+
+; Java injection in JSP expressions - these contain Java expressions
+(jsp_expression) @injection.content
+(#set! injection.language "java")
+
+; Java injection in JSP declarations - these contain Java declarations (fields, methods)
+(jsp_declaration) @injection.content
+(#set! injection.language "java")
+
+; Expression Language (EL) injection
+; EL is a simple expression language, we'll inject as Java for basic syntax highlighting
+; Individual editors can override this with EL-specific parsers if available
+(el_expression) @injection.content
+(#set! injection.language "java")
+(#set! injection.include-children)
