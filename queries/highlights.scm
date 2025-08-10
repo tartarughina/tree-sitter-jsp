527dff4962bf47a9bad0814d4db172e6aadb9090
Riccardo Strina <rstrina@amazon.com>
2025-08-10 11:16:45 +0200

    Update .scm queries

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,11 +1,34 @@
+; JSP Directives
 (jsp_directive) @property
 (jsp_directive_name) @keyword
 
-(attribute) @number
+; Other JSP elements get the color from the injected language
+
+; Comments
+(comment) @comment
+(jsp_comment) @comment
+
+; HTML Elements
+(tag_name) @tag
+((tag_name) @keyword
+ (#match? @keyword ":"))
+
+(attribute_name) @attribute
+
+(attribute_value) @string
 (quoted_attribute_value) @string
 
-(tag_name) @constant
-((tag_name) @module
- (#match? @module ":"))
+; Operators and punctuation
+[
+  "<"
+  ">"
+  "</"
+  "/>"
+] @punctuation.delimiter
 
-["<" ">" "/>" "</"] @operator
+; Text content
+(text) @text
+(raw_text) @text
+
+; Interpolation
+(interpolation) @embedded
--- queries/injections.scm
+++ queries/injections.scm
@@ -1,50 +1,26 @@
-; JavaScript injection in script tags with explicit type attribute
-((script_element
-  (start_tag
-    (attribute
-      (attribute_name) @_attr
-      (quoted_attribute_value
-        (attribute_value) @injection.language)))
-  (raw_text) @injection.content)
- (#eq? @_attr "type")
- (#match? @injection.language "(text/)?javascript"))
-
-; JavaScript injection in script tags (default - no type attribute needed)
-((script_element
-  (raw_text) @injection.content)
- (#set! injection.language "javascript"))
-
-; CSS injection in style tags with explicit type attribute  
-((style_element
-  (start_tag
-    (attribute
-      (attribute_name) @_attr
-      (quoted_attribute_value
-        (attribute_value) @injection.language)))
-  (raw_text) @injection.content)
- (#eq? @_attr "type")
- (#match? @injection.language "(text/)?css"))
-
-; CSS injection in style tags (default - no type attribute needed)
-((style_element
-  (raw_text) @injection.content)
- (#set! injection.language "css"))
-
 ; Java injection in JSP scriptlets - these contain Java code blocks
-(jsp_scriptlet) @injection.content
-(#set! injection.language "java")
+((jsp_scriptlet) @injection.content
+ (#set! injection.language "java"))
 
 ; Java injection in JSP expressions - these contain Java expressions
-(jsp_expression) @injection.content
-(#set! injection.language "java")
+((jsp_expression) @injection.content
+ (#set! injection.language "java"))
 
 ; Java injection in JSP declarations - these contain Java declarations (fields, methods)
-(jsp_declaration) @injection.content
-(#set! injection.language "java")
+((jsp_declaration) @injection.content
+ (#set! injection.language "java"))
 
 ; Expression Language (EL) injection
 ; EL is a simple expression language, we'll inject as Java for basic syntax highlighting
 ; Individual editors can override this with EL-specific parsers if available
-(el_expression) @injection.content
-(#set! injection.language "java")
-(#set! injection.include-children)
+((el_expression) @injection.content
+ (#set! injection.language "java")
+ (#set! injection.include-children))
+
+(style_element
+    (raw_text) @injection.content
+    (#set! injection.language "html"))
+
+(script_element
+    (raw_text) @injection.content
+    (#set! injection.language "js"))
